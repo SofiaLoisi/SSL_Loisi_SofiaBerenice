450c6e5307571a7b8805059a8e466b688cc560a7
Sofia <sloisi@frba.utn.edu.ar>
2026-04-02 21:34:20 -0300

    agrego archivo principal + hola mundo

---
+++ 00-PrimerProgramaC/archivo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+//COMPILAR CON: gcc archivo.c -o archivo.exe
+// archivo.c es el archivo fuente, archivo.exe es el de salida
+
+//EJECUTAR CON: ./archivo.exe
+
+
+int main (int argc, char *argv[]){
+    printf("Hola Mundo\n");
+}
